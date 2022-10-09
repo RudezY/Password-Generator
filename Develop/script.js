0ee77ee2001247877d673601a187f0d8dab40ad4
RudezY <rudyyamashiro@yahoo.com>
2022-10-08 23:09:03 -0700

    added Variables for numbers and characters as well as the number generator for picking characters

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,40 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Alphabet, Numbers, Special Characters
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercase = uppercase.map(letter => letter.toLowerCase())
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "<", ">", "/", "?", "~", "`"]
+
+// This function generates a random number within each variable to choose which character or number will be picked. Rounded to the lowest integer
+
+function getRandomInt(max) {
+  return Math.floor(math.random() * max)
+}
+
+
+// Function to generate password
+
+function generatePassword() {
+  // This will ask how many characters are wanted for the user password
+  var passwordLength = prompt("How many characters would you like your password to be? ( Between 8 - 128 characters )")
+  // This will ask if the user wants to use uppercase letters in the password
+  var useUppercase = confirm("Would you like to include uppercase letters in your password?")
+  // This will ask if the user wants to use lowercase letters in the password
+  var useLowercase = confirm("Would you like to use lowercase letters in your password?")
+  // This will ask if the user wants to use numbers in the password
+  var useNumbers = confirm("Would you like to include numbers in your password?")
+  // This will ask if the user wants to use special characters in the password
+  var useSpecialChars = confirm("Would you like to include special characters in your password?")
+
+
+
+
+
+
+
+
+}
+
